e0c3fcf4eb0f82e0c69d271f2da7a8fa308ec991
itayoren <itayor053@gmail.com>
2022-03-25 01:39:06 +0200

    first try

--- PI_clac.cpp
+++ PI_clac.cpp
@@ -3,55 +3,35 @@
 
 using namespace std;
 
-
-//void setup() {
-//    size(402, 402);
-//    background(0);
-//    translate(width/2, height/2);
-//    stroke(255);
-//    strokeWeight(4);
-//    noFill();
-//    ellipse(0, 0, r*2, r*2);
-//    rectMode(CENTER);
-//    rect(0, 0, r*2, r*2);
-//}
-
-int main() {
-    cout << "test\n";
-    int r = 200;
-
-    int total = 0;
-    int circle = 0;
-
-    double recordPI = 0;
-    srand(time(NULL));
-
-    while(true) {
-        for (int i = 1; i < 10000; i++) {
-            for (int j = 1; j < 10000; ++j) {
-//            float x = rand() % (-r - r + 1) + r;
-//            float y = rand() % (-r - r + 1) + r;
-                float x = i;
-                float y = j;
-                total++;
-
-                double d = (double) x * (double) x + (double) y * (double) y;
-                if (d < (double) r * (double) r) {
-                    circle++;
-                }
-
-
-                double pi = (double) 4 * ((double) circle / (double) total);
-                cout << pi << endl;
-
-                double recordDiff = abs(M_PI - recordPI);
-                double diff = abs(M_PI - pi);
-                if (diff < recordDiff) {
-                    recordDiff = diff;
-                    recordPI = pi;
-                    cout << recordPI << endl;
-                }
-            }
+double pi_calc(int n) {
+    int c = 0;
+    double pi = 0;
+    int a;
+    for (a = 1; a <= n; a = a + 2) {
+        if (c == 0) {
+            pi = pi + 4 / (float) a;
+            c++;
+        } else {
+            pi = pi - 4 / (float) a;
+            c--;
         }
     }
+    return pi;
+}
+
+int main() {
+    //declare variables
+    double sum, pi;
+    int n;
+
+    //prompt user
+    cout << "How many decimals would you like to calculate pi to? ";
+    cin >> n;
+
+    //calculate pi
+
+    //print response
+    cout << pi_calc(n) << endl;
+
+    return 0;
 }
